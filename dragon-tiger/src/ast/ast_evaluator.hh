d8f017460917e1c963818aba6dc8c4207473346c
Yaroslav Fedyna <fedynaslava@gmail.com>
2024-05-27 18:46:53 +0300

    fixed

--- dragon-tiger/src/ast/ast_evaluator.hh
+++ dragon-tiger/src/ast/ast_evaluator.hh
@@ -13,19 +13,20 @@ public:
     ASTEvaluator() : result(0) {} // Initialize result to 0
 
     int32_t visit(const IntegerLiteral &);
-    // int32_t visit(const StringLiteral &);
     int32_t visit(const BinaryOperator &);
     int32_t visit(const Sequence &);
-    // int32_t visit(const Let &);
-    // int32_t visit(const Identifier &);
     int32_t visit(const IfThenElse &);
-    // int32_t visit(const VarDecl &);
-    // int32_t visit(const FunDecl &);
-    // int32_t visit(const FunCall &);
-    // int32_t visit(const WhileLoop &);
-    // int32_t visit(const ForLoop &);
-    // int32_t visit(const Break &);
-    // int32_t visit(const Assign &);
+
+    int32_t visit(const StringLiteral &);
+    int32_t visit(const Let &);
+    int32_t visit(const Identifier &);
+    int32_t visit(const VarDecl &);
+    int32_t visit(const FunDecl &);
+    int32_t visit(const FunCall &);
+    int32_t visit(const WhileLoop &);
+    int32_t visit(const ForLoop &);
+    int32_t visit(const Break &);
+    int32_t visit(const Assign &);
 
     // Default handler for other nodes
     void visit(const Node &) override {
